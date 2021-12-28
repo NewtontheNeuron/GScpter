044f013b70b4f92b86fa081ab4fa56e5ac84db9c
Justin <justin.bellavance@gmail.com>
2021-12-27 19:31:20 -0500

    file flexibility for 2 and 4 subgroups

--- Scripts/JSON_Handler.r
+++ Scripts/JSON_Handler.r
@@ -5,17 +5,23 @@ library(rjson)
 setwd(dirname(getActiveDocumentContext()$path))
 
 #get yaml/json data. Only works if current directory is changed.
-Data <- fromJSON(file = "../Data/JSON/Example.json")
+Data <- fromJSON(file = "../Data/JSON/Example_2_Subgroups.json")
 
 #return list of features from JSON object.
 returnFeatures <- function(){
     return(Data$features)
 }
 
+returnClusterpool_names <- function(){
+    return(Data$clusterpool_names)
+}
+
+returnClusterpool_subgroups <- function(){
+    return(Data$subgroup_names)
+}
+
 #return list of genes to look at according to specification from JSON object.
 #TODO: Add error handling (i.e. what happens when the cell that was called doesn't exist.)
-returnClusterpoolGenes("excitatory", "DDH")
-
 returnClusterpoolGenes <- function(Type, Name){
     listOfGenes <- Data[["clusterpools"]][[Type]][[Name]]
     return(listOfGenes)
