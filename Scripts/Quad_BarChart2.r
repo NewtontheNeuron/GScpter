33794564d546b59a2748e99cf30a0b343798fb08
Justin <justin.bellavance@gmail.com>
2022-01-12 19:57:32 -0500

    universal scales for Quad_BarPlot

--- Scripts/Quad_BarChart2.r
+++ Scripts/Quad_BarChart2.r
@@ -77,13 +77,13 @@ Run_ANOVA <- function (cluster, method, anova.p.val=NULL) {
 }
 
 #create individual plot with wanted options.
-Plot_details <- function (avg, clusterpool, clusterpool_exp, method_exp, title, method, label){
+Plot_details <- function (avg, clusterpool, clusterpool_exp, method_exp, title, method, label, y_lim){
     ggplot(avg, aes(features.plot, method_exp, fill = factor(features.plot))) + 
       geom_col(color="black", show.legend = FALSE) + 
       scale_fill_viridis_d(option = "plasma") + 
       geom_errorbar(aes(ymin = lower, ymax = upper), position = position_dodge(width=0.9), width = 0.50) +
       geom_point(data = clusterpool, y = clusterpool_exp, fill = "white", color="black") +
-      scale_y_continuous(expand = c(0,0), limits = c(0, NA)) + 
+      scale_y_continuous(expand = c(0,0), limits = c(0, y_lim)) + 
       labs(x=paste(label, "Genes"), y=title) +
       cowplot::theme_cowplot() + 
       scale_x_discrete(labels=clean_label_list) +
@@ -96,14 +96,14 @@ Plot_details <- function (avg, clusterpool, clusterpool_exp, method_exp, title, 
 # Bargraphs for the average expression and percent expressed data, and also for both clusterpools
 Plot_4_Bar <- function (avg1, pct1, avg2, pct2, c1, c2, labels){
 
-  Plot_1 <- Plot_details(avg1, c1, c1$avg.exp, avg1$avg.exp, "Average Expression", "avg", labels[1]) 
+  Plot_1 <- Plot_details(avg1, c1, c1$avg.exp, avg1$avg.exp, "Average Expression", "avg", labels[1], 12) 
 
-  Plot_2 <- Plot_details(avg2, c2, c2$avg.exp, avg2$avg.exp, "Average Expression", "avg", labels[2])
+  Plot_2 <- Plot_details(avg2, c2, c2$avg.exp, avg2$avg.exp, "Average Expression", "avg", labels[2], 12)
   Plot_2 <- Plot_Bween(Plot_2, Bween_pool("avg", c1, c2), "avg", c2)
   
-  Plot_3 <- Plot_details(pct1, c1, c1$pct.exp, pct1$pct.exp, "%Expressed", "pct", labels[1])
+  Plot_3 <- Plot_details(pct1, c1, c1$pct.exp, pct1$pct.exp, "%Expressed", "pct", labels[1], 100)
   
-  Plot_4 <- Plot_details(pct2, c2, c2$pct.exp, pct2$pct.exp, "%Expressed", "pct", labels[2])
+  Plot_4 <- Plot_details(pct2, c2, c2$pct.exp, pct2$pct.exp, "%Expressed", "pct", labels[2], 100)
   Plot_4 <- Plot_Bween(Plot_4, Bween_pool("pct", c1, c2), "pct", c2)
   
   Plot <- Plot_1 + Plot_2 + Plot_3 + Plot_4
